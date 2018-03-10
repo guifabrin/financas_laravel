7c9d21bef8f86ce63311093a52c099a6bc42b97d
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2018-03-10 15:54:43 -0300

    Add Doctrine DBAL, update users_table and do login work.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,85 +2,95 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
+use Log;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
 use App\UserOauth;
+use App\User;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles authenticating users for the application and
+  | redirecting them to your home screen. The controller uses a trait
+  | to conveniently provide its functionality to your applications.
+  |
+  */
 
-    use AuthenticatesUsers;
+  use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+  /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('guest')->except('logout');
+  }
 
-    /**
-     * Redirect the user to the Facebook authentication page.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function redirectToProvider($provider)
-    {
-        return \Socialite::driver($provider)->redirect();
-    }
+  /**
+   * Redirect the user to the Facebook authentication page.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function redirectToProvider($provider)
+  {
+    return \Socialite::driver($provider)->redirect();
+  }
 
-    /**
-     * Obtain the user information from Facebook.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function handleProviderCallback($provider)
-    {
-        $userSocialite = \Socialite::driver($provider)->user();
-        $id = $userSocialite->getId();
-        $name = $userSocialite->getName();
-        $email = $userSocialite->getEmail();
-        $avatar = $userSocialite->getAvatar();
-        $userOauth = UserOauth::where('uuid',$userSocialite->getId())->first();
-        if ($userOauth == null){
-            $user = User::where('email',$userSocialite->getEmail())->first();
-            if ($user==null){
-                $user = new User;
-                $user->name = $userSocialite->getName();
-                $user->email = $userSocialite->getEmail();
-                $user->picture = $userSocialite->getAvatar();
-                $user->save();
-            }
-            $userOauth = new UserOauth;
-            $userOauth->uuid = $userSocialite->getId();
-            $userOauth->name = $userSocialite->getName();
-            $userOauth->email = $userSocialite->getEmail();
-            $userOauth->avatar = $userSocialite->getAvatar();
-            $userOauth->user_id = $user->id;
-            
-        } else {
-
+  /**
+   * Obtain the user information from Facebook.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function handleProviderCallback($provider)
+  {
+    $userSocialite = \Socialite::driver($provider)->user();
+    $userOauth = UserOauth::where('uuid',$userSocialite->getId())->first();
+    if ($userOauth == null){
+      Log::info('User Oauth not found');
+      if(Auth::guest()){
+        Log::info('User not logged');
+        $user = User::where('email',$userSocialite->getEmail())->first();
+        if ($user==null){
+          $user = new User;
+          $user->name = $userSocialite->getName();
+          $user->email = $userSocialite->getEmail();
+          $user->picture = $userSocialite->getAvatar();
+          $user->save();
         }
+      } else {
+        Log::info('User logged');
+        $user = Auth::user();
+      }
+      $userOauth = new UserOauth;
+      $userOauth->uuid = $userSocialite->getId();
+      $userOauth->name = $userSocialite->getName();
+      $userOauth->email = $userSocialite->getEmail();
+      $userOauth->avatar = $userSocialite->getAvatar();
+      $userOauth->provider = $provider;
+      $userOauth->user()->associate($user);
+      $userOauth->save();
     }
+    Auth::login($userOauth->user);
+    Log::info(['User Oauth', $userOauth->toJson(), $userOauth->user->toJson(), Auth::check()]);
+    return redirect($this->redirectTo);
+  }
 }
--- app/UserOauth.php
+++ app/UserOauth.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserOauth extends Model
 {
-    //
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'avatar', 'uuid'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
