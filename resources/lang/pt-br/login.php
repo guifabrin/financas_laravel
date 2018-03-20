6c48d532c73c1592cb8723cbc35f9d645aae26b6
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2018-03-20 19:23:17 -0300

    Ajuste de layout.

---
+++ resources/lang/pt-br/login.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'email' => 'Email',
+    'password' => 'Senha',
+    'remember_me' => 'Lembrar-me',
+    'login' => 'Entrar',
+    'forgot' => 'Esqueci minha senha',
+    'name' => 'Nome',
+    'password_confirmation' => 'Confirmar senha',
+    'register' => 'Registrar',
+    'reset_password' => 'Recuperar senha',
+    'send_email_reset' => 'Enviar email'
+  ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 control-label">{{__('login.name')}}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">{{__('login.email')}}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">{{__('login.password')}}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 control-label">{{__('login.password_confirmation')}}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -64,11 +64,19 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    {{__('login.register')}}
                                 </button>
                             </div>
                         </div>
                     </form>
+                    
+                    <div class="row">
+                        <div class="col-md-3 col-md-offset-5">
+                            <a href="{{ url('login/facebook') }}" class="btn btn-block btn-social btn-facebook">
+                              <span class="fa fa-facebook"></span> Facebook
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
