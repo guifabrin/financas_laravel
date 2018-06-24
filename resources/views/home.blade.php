4c436120dc2de6aa13b6cf6ebc2900c0eaee89f1
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2018-06-24 15:20:18 -0300

    Add video on home.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,5 @@
 @endsection
 
 @section('content')
-	{!!__('home.content')!!}
+<iframe height="500" src="https://www.youtube.com/embed/f4FmbfLHYF8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="width: 100%;"></iframe>
 @endsection
